f28c31ff15eb1cbeb96497445915f1a04e229e5d
DavidIntriago <david.intriago@unl.edu.ec>
2024-04-09 21:47:00 -0500

    modelo planta

--- back-vivero/app/models/planta.js
+++ back-vivero/app/models/planta.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
     nombre: {type: DataTypes.STRING(150), defaultValue:"NONE"},
     alias: {type: DataTypes.STRING(150), defaultValue:"NONE"},
     tamanio: {type: DataTypes.STRING(30), defaultValue:"NONE"},
-    descripcion: {type: DataTypes.TEXT('long'), defaultValue:"NONE"},
+    descripcion: {type: DataTypes.STRING, defaultValue:"NONE"},
     foto:{type: DataTypes.STRING, defaultValue: "sin_foto.jpg"},
     external_id:{type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4}
   }, { timestamps: false, freezeTableName: true });
