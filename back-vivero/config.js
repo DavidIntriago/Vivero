ed211fdd0ee50663c3e27cccdfd73f3577848cf7
DavidIntriago <david.intriago@unl.edu.ec>
2024-04-09 21:30:04 -0500

    puerto

--- back-vivero/config.js
+++ back-vivero/config.js
@@ -2,6 +2,7 @@ const database = process.env.database || "vivero";
 const username = process.env.username || "user";
 const password = process.env.password || "user";
 const host = process.env.host || "127.0.0.1";
+const port= process.env.port;
 const dialect = process.env.dialect || "mysql";
 const FRONT_END_URL = process.env.FRONT_END_URL || "http://localhost:3001";
 const operatorAliases = process.env.operatorAliases || false;
@@ -12,6 +13,7 @@ module.exports = {
   password,
   host,
   dialect,
+  port,
   operatorAliases,
   FRONT_END_URL
 };
